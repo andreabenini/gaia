a97917e5f2599209801ad45d2641221aff3f79d9
Ben <andreabenini@users.noreply.github.com>
2021-12-24 16:01:10 +0100

    Adding TLS security - added SSL/TLS layer - chatengine logging utilities - cert auth checking - improved debugging and error checking

--- botserver/chatengine.py
+++ botserver/chatengine.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Bare bone chat engine
+# Barebone chat engine
 # @see:
-#     self.valid    Second stage constructor
+#       self.valid          Second stage constructor
+#       self.message()      Send a message to this engine to receive a reply
 #
 
 # Program imports
 try:
-    import yaml
+    import os
+    import csv
+    import datetime
 except ModuleNotFoundError as E:
     print(f"{E}. Install required modules.")
 
@@ -17,11 +20,45 @@ class chatEngine():
     @property
     def valid(self):
         return self._valid
+    @property
+    def error(self):
+        return self._error
 
-    def __init__(self):
-        self._valid = True
+    # Class constructor
+    def __init__(self, pathDatabase=None):
+        try:
+            self._path  = pathDatabase
+            self._logInit()
+            self._logWrite("Engine initialized")
+            self._valid = True
+        except Exception as E:
+            self._error = str(E).strip()
+            self._valid = False
 
+    # Logging methods
+    def _logInit(self):
+        filename = self._path + os.path.sep + 'chat.log'
+        self._logFile = None
+        csvfile = open(filename, 'a+')
+        self._logFile = csv.writer(csvfile, delimiter='|')
+        if self._logFile == None:
+            raise ValueError(f"Cannot open '{filename}' for writing")
+    def _logWrite(self, message1='', message2=None, msgtype='SYSTEM'):
+        logline = [datetime.datetime.strftime(datetime.datetime.now(), '%Y/%m/%d %H:%M:%S'), msgtype, message1]
+        if message2:
+            logline += [message2]
+        self._logFile.writerow(logline)
+
+
+    # Send a <message> to <username>
+    # @param  username (String) Referred user
+    # @param  message  (String) Dedicated message
+    #
+    # @return (None)   If message is invalid
+    # @return (String) Replied message
     def message(self, username=None, message=None):
         if not username or not message:
+            self._valid = False
             return None
+        # just a stub. Dummy output until I get something useful
         return f"{username}: {message}"
--- botserver/config.py
+++ botserver/config.py
@@ -22,16 +22,17 @@ class serverConfiguration():
         args = parser.parse_args()
         self._errorMessage = ''
         self._filename = args.configuration
-        self._property = None
+        self._path = self._pathDB = self._property = None
         self._valid = False
         try:
             # yaml loading
             with open(self.filename, 'r') as fHandler:
                 config = yaml.safe_load(fHandler)
-            if 'botserverHost' not in config: raise Exception("[botserverHost] not found in configuration file")
-            if 'botserverPort' not in config: raise Exception("[botserverPort] not found in configuration file")
+            if 'botserverHost'    not in config: raise Exception("[botserverHost] not found in configuration file")
+            if 'botserverPort'    not in config: raise Exception("[botserverPort] not found in configuration file")
+            if 'allowedClients'   not in config: raise Exception("[allowedClients] not found in configuration file")
+            if 'botserverTimeout' not in config: config['botserverTimeout'] = 30
             self._property = config
-
             # Checking existence of Server and CA certs
             dirCertificates = os.path.dirname(self.filename)
             dirCertificates = ('.' if dirCertificates=='' else dirCertificates) + os.path.sep + 'certs' + os.path.sep
@@ -40,11 +41,24 @@ class serverConfiguration():
             self.serverCertificate  = self.__checkFile(dirCertificates, "server_cert.pem")
             self.serverKey          = self.__checkFile(dirCertificates, "server_key.pem")
 
+            self.__initFileSystem()                         # Program path and dir init
+
         except Exception as E:
             self._errorMessage = str(E)
             return
         # object loaded successfully
         self._valid = True
+
+    def __initFileSystem(self):
+        try:
+            self._pathDB = os.path.dirname(self.filename)
+            self._pathDB = ('.' if self._pathDB=='' else self._pathDB) + os.path.sep + 'db'
+            if not os.path.exists(self.pathDatabase):
+                os.mkdir(self.pathDatabase)
+            if not os.path.isdir(self.pathDatabase):
+                raise ValueError(f'{self.pathDatabase} is not a directory')
+        except Exception as E:
+            raise ValueError(str(E))
 
     def __checkFile(self, dir, filename):
         if os.path.exists(dir + filename):
@@ -52,6 +66,12 @@ class serverConfiguration():
         else:
             raise Exception(f"File  {dir}{filename}  not found")
 
+    @property
+    def path(self):
+        return self._path
+    @property
+    def pathDatabase(self):
+        return self._pathDB
     @property
     def filename(self):
         return self._filename

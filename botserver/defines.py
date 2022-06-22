247d6e068086c6340302df5ed7bfd0c534d0c917
Ben <andreabenini@users.noreply.github.com>
2022-06-23 00:35:23 +0200

    Improved trainer Lemmatizer and improved object storage

--- botserver/defines.py
+++ botserver/defines.py
@@ -1,8 +1,8 @@
 # local defines
-FILE_CONFIG  = 'config.yaml'
-FILE_MODEL   = 'model.h5'
-FILE_WORDS   = 'words.pkl'
-FILE_CLASSES = 'classes.pkl'
+FILE_CONFIG    = 'config.yaml'
+FILE_MODEL     = 'model.h5'
+FILE_WORDS     = 'words.pkl'
+FILE_CLASSES   = 'classes.pkl'
 
 # Variables recognition regexp
-REGEX = r"\{\{([A-Za-z0-9,%:\+\-\ ]+)\}\}"            # pattern match for {{vars}}
+REGEX = r"\{\{([A-Za-z0-9,%:\+\-\ ]+)\}\}"              # pattern match for {{vars}}

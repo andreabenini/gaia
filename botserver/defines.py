fe6b63b94b910f17d4e4e65a645562bf10351d0e
Ben <andreabenini@users.noreply.github.com>
2022-06-28 23:35:42 +0200

    Adding pattern

--- botserver/defines.py
+++ botserver/defines.py
@@ -5,5 +5,5 @@ FILE_WORDS     = 'words.pkl'
 FILE_CLASSES   = 'classes.pkl'
 
 # Variables recognition
-REGEX          = r"\{\{([A-Za-z0-9,%:\+\-\ ]+)\}\}"         # pattern match for {{vars}}
+REGEX          = r"\{\{([A-Za-z0-9,\*%:\+\-\ ]+)\}\}"         # pattern match for {{vars}}
 IGNORE_WORDS   = ['?', '!', ',']                            # Ignore these words from learning process
